923b277915537da4a150bfa5deac120302471833
MeAqua1420 <avawaava@gmail.com>
2024-06-27 23:00:50 +0800

    6.27

--- app.py
+++ app.py
@@ -34,19 +34,22 @@ class Algorithm(db.Model):
     upload_date = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
     type = db.Column(db.String(255),nullable = False)
 
-
-
-@app.route('/register', methods=['POST'])
-def register_user():
-    try:
+class DBaction:
+    def AddUser(self):
         username = request.form['username']
         password = request.form['password']
         phone = request.form['phone']
+
         logging.info(f"Received registration data: username={username}, phone={phone}")
         new_user = User(username=username, password=password, phone=phone)
         db.session.add(new_user)
         db.session.commit()
-        logging.info(f"User {username} registered successfully.")
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    try:
+        DBaction.AddUser(act)
+        logging.info(f"User {request.form['username']} registered successfully.")
 
         return render_template('error5.html', message="注册成功")
     except IntegrityError as e:
@@ -61,11 +64,8 @@ def register_user():
 @app.route('/login', methods=['POST'])
 def login_user():
     try:
-        print(request.form)
         username = request.form['username']
         password = request.form['password']
-        print("username ="+username)
-        print("password =" + password)
         logging.info(f"Received login data: username={username}")
         user = User.query.filter_by(username=username, password=password).first()
         if user:
@@ -96,12 +96,36 @@ def overview():
 
 @app.route('/search', methods=['GET'])
 def search():
-    sort_count = Algorithm.query.filter_by(type='SORT').count()
-    search_count = Algorithm.query.filter_by(type='Search').count()
-    math_count = Algorithm.query.filter_by(type='Math').count()
-    graph_count = Algorithm.query.filter_by(type='Graph').count()
+    query = request.args.get('query')
+    results = Algorithm.query.filter(
+        (Algorithm.name.ilike(f'%{query}%')) | (Algorithm.type.ilike(f'%{query}%'))
+    ).all()
+    results_list = [{'name': alg.name, 'type': alg.type} for alg in results]
+    return jsonify(results_list)
 
-    return render_template("Search.html",data1 = [sort_count, search_count, math_count, graph_count])
+@app.route('/searchpage', methods=['GET'])
+def searchpage():
+    return render_template("hello.html")
+
+@app.route('/algorithm', methods=['GET'])
+def get_algorithm():
+    print("hello")
+    name = request.args.get('name')
+    print(name)
+    # algorithm = Algorithm.query.get(id)
+    # print(id)
+    # if algorithm:
+    #     return jsonify({
+    #         'name': algorithm.name,
+    #         'type': algorithm.type,
+    #         'time_complexity': algorithm.time_complexity,
+    #         'uploader': algorithm.uploader,
+    #         'storage_location': algorithm.storage_location,
+    #         'upload_date': algorithm.upload_date.strftime('%Y-%m-%d %H:%M:%S')
+    #     })
+    # else:
+    #     return jsonify({'error': 'Algorithm not found'}), 404
+    return jsonify({'error': 'Algorithm not found'}), 404
 @app.route('/reset_password', methods=['POST'])
 def reset_password():
     try:
@@ -132,6 +156,7 @@ def reset_password():
 
 
 if __name__ == '__main__':
+    act = DBaction()
     with app.app_context():
         db.create_all()
     app.run(debug=True, use_reloader=False, port=6699)
